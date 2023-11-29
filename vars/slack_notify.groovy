0e600a03ec209901b4f0407fe5ba030d12eaad25
Viswajith Ramesh <63919015+vj4a@users.noreply.github.com>
2023-11-29 10:21:35 +0530

    Update slack_notify.groovy

--- vars/slack_notify.groovy
+++ vars/slack_notify.groovy
@@ -20,7 +20,7 @@ def call(String buildStatus, String release_tag=null, String jobName=null, int b
                 if (jobName == null)
                     jobName = env.JOB_NAME
                 if (buildNumber == 0)
-                    buildNumber = env.BUILD_NUMBER
+                    buildNumber = env.BUILD_NUMBER.toInteger()
                 if (jobUrl == null)
                     jobUrl = env.JOB_URL
 
